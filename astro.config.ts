e46ad0ebc59a7344ed4df23762180f608ece76e0
Marcos Caramalho <marcos.caramalho@gmail.com>
2022-11-25 11:58:59 +0000

    add new post

--- astro.config.ts
+++ astro.config.ts
@@ -4,16 +4,22 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+
 export default defineConfig({
 	markdown: {
 		shikiConfig: {
 			theme: "dracula",
 			wrap: false,
-		},
+		}
 	},
 	site: "https://mcaramalho.com",
 	integrations: [
-		mdx({}),
+		mdx({
+			remarkPlugins: [remarkMath],
+			rehypePlugins: [rehypeKatex]
+		}),
 		tailwind({
 			config: { applyBaseStyles: false },
 		}),
