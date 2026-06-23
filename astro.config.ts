76e56eb8a0258092f511cf01460cd039febe352f
marcos <marcos.caramalho@gmail.com>
2026-06-23 23:05:42 +0100

    adds new post

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,21 @@ import sitemap from "@astrojs/sitemap";
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 
+function remarkMermaid() {
+	return (tree: any) => {
+		function walk(node: any) {
+			if (node.type === 'code' && node.lang === 'mermaid') {
+				node.type = 'html';
+				node.value = `<pre class="mermaid">${node.value}</pre>`;
+			}
+			if (node.children) {
+				node.children.forEach(walk);
+			}
+		}
+		walk(tree);
+	};
+}
+
 export default defineConfig({
 	markdown: {
 		shikiConfig: {
@@ -17,7 +32,7 @@ export default defineConfig({
 	site: "https://r3zz.io",
 	integrations: [
 		mdx({
-			remarkPlugins: [remarkMath],
+			remarkPlugins: [remarkMath, remarkMermaid],
 			rehypePlugins: [rehypeKatex]
 		}),
 		tailwind({
